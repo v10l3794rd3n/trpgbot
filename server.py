6e4fb1dc8d9154b76540486bedf1d8ab04304ed9
orangegamza <orangegamza@gmail.com>
2025-02-17 22:02:55 +0900

    attendance

--- server.py
+++ server.py
@@ -10,8 +10,8 @@ from mastodon.streaming import StreamListener
 
 # Create an instance of the Mastodon class
 mastodon = Mastodon(
-    access_token='roCGE6NPcKrN90zk2Z5rg0-TnDwyuHP7wzH5dJ3qFq4',
-    api_base_url='https://strivings.life'
+    access_token='e-4WxIYsokSRDMCfSQe1Q1PHXiWi8REdZaThWxYLeys',
+    api_base_url='https://ellipsishgwt.com'
 )
 
 
@@ -23,15 +23,13 @@ class dgListener(StreamListener):
         if notification['type'] == 'mention':
             id = notification['status']['id']
             visibility = notification['status']['visibility']
-            if '[바깥일]' in notification['status']['content']:
-                answers = '나가려고? 어디로?'
-            elif '[집안일]' in notification['status']['content']:
+            if '[출석]' in notification['status']['content']:
                 answers = script.make_daily_script(notification['account']['username'])
             else:
-                answers = "무언가 잘못된 것 같다. 다시 시도해 보자. @bunker"
+                answers = "무언가 잘못된 것 같다. 다시 시도해 보자. @ellipsis"
                 visibility = 'direct'
         
-        mastodon.status_post("@" + notification['account']['username'] + "\n" + 
+        mastodon.status_post("@" + notification['account']['username'] + "  " + 
                             answers, in_reply_to_id = id, 
                             visibility = visibility)
 
